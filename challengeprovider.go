5893774d3768f422cbde978b8b2674ba378f74fe
Daniel R. Kumor <daniel@dkumor.com>
2016-08-29 00:02:46 -0400

    Updated ChallengeProvider
    Since acmewrapper came out early this year, overloading the SNI ChallengeProvider has become a de-facto standard of updating certificates during runtime. I like to think that people used acmewrapper's code as a reference when implementing other libraries :). Since then, the lego library has made it easier to access the domain name for use in SNI challenge. This commit moves to this new method of accessing SNI domains, instead of manually parsing the x509 certificate.

--- challengeprovider.go
+++ challengeprovider.go
@@ -1,46 +1,29 @@
 package acmewrapper
 
-import (
-	"crypto/tls"
-	"crypto/x509"
-
-	"github.com/xenolf/lego/acme"
-)
-
-// The ChallengeProvider fits the interface of lego/acme used for challenges.
+import "github.com/xenolf/lego/acme"
 
 // wrapperChallengeProvider is used to fit into the acme.ChallengeProvider interface,
-// which allows us to use our own SNI code that is already being used to live-update
-// our TLS certificates in the challenge process too
+// which allows us to modify our server during runtime to solve the SNI challenge
 type wrapperChallengeProvider struct {
-	w    *AcmeWrapper
-	cert *tls.Certificate
+	w               *AcmeWrapper
+	challengeDomain string
 }
 
 // Present sets up the challenge domain thru SNI. Part of acme.ChallengeProvider interface
 func (c *wrapperChallengeProvider) Present(domain, token, keyAuth string) error {
 	logf("[acmewrapper] Started SNI server modification for %s", domain)
+
 	// Use ACME's SNI challenge cert maker. How nice that it is exported :)
-	cert, _, err := acme.TLSSNI01ChallengeCert(keyAuth)
+	cert, challengedomain, err := acme.TLSSNI01ChallengeCert(keyAuth)
 	if err != nil {
 		return err
 	}
 
-	// The returned cert has the info we want, but not parsed - so parse it
-	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
-	if err != nil {
-		return err
-	}
+	// Add the cert to our AcmeWrapper. here, the names is the special SNI challenge domain
+	// in the form "<Zi[0:32]>.<Zi[32:64]>.acme.invalid"
+	c.w.AddSNI(challengedomain, &cert)
 
-	// The cert maker function gives us all the info we need in the cert itself
-	for i := range cert.Leaf.DNSNames {
-
-		// Add the cert to our AcmeWrapper. here, the names is the special SNI challenge domain
-		// in the form "<Zi[0:32]>.<Zi[32:64]>.acme.invalid"
-		c.w.AddSNI(cert.Leaf.DNSNames[i], &cert)
-	}
-
-	c.cert = &cert
+	c.challengeDomain = challengedomain
 
 	return nil
 
@@ -49,8 +32,6 @@ func (c *wrapperChallengeProvider) Present(domain, token, keyAuth string) error 
 // CleanUp removes the challenge domain from SNI. Part of acme.ChallengeProvider interface
 func (c *wrapperChallengeProvider) CleanUp(domain, token, keyAuth string) error {
 	logf("[acmewrapper] End of SNI server modification for %s\n", domain)
-	for i := range c.cert.Leaf.DNSNames {
-		c.w.RemSNI(c.cert.Leaf.DNSNames[i])
-	}
+	c.w.RemSNI(c.challengeDomain)
 	return nil
 }
